261d1547e300ebb8d3c86ea63291c912796ab5c3
kevinkl3 <kevinlopez@unitec.edu>
2013-12-01 14:08:15 -0600

    A little doc

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+/* -----------------------------------------------------\
+| 		  Turing Machines Computation Simulator			|
+|	Authors:  		 									|
+|	Kevin Leonel Lopez 	- kevinlopez@unitec.edu			|
+|	Oseas Andres Torrez - andrews_torres@hotmail.com 	|
+\------------------------------------------------------*/
+
 /*function dividir(){
 	var s=$("#Q_estados").val();
 	var Q=separar(s);
@@ -29,22 +36,22 @@
 	console.log(q0);
 	console.log(qA);
 	console.log(qR);
-	
 
 }*/
-	function validate(A,s){
-		for (var i = 0; i <A.length; i++) {
-			if(A[i]==s){
-				return true;
-			}
+function validate(A,s){
+	for (var i = 0; i < A.length; i++) {
+		if(A[i]==s){
+			return true;
 		}
-		return false;
 	}
-	function separar(s){
-		var s=s.replace(/ /g,"");
-		var A=s.split(",");
-		return A;
-	}
+	return false;
+}
+
+function separar(s){
+	var s=s.replace(/ /g,"");
+	var A=s.split(",");
+	return A;
+}
 
 $( "#Q_estados" ).change(function() {
   var Sq0=$("#q0_inicial");
@@ -58,7 +65,6 @@ $( "#Q_estados" ).change(function() {
   qs.empty();
   qs1.empty();
   var Q=separar($("#Q_estados").val());
-  
   for (var i = 0; i<Q.length; i++) {
   	Sq0.append(new Option(""+Q[i],i));
   	SqA.append(new Option(""+Q[i],i));
@@ -69,6 +75,7 @@ $( "#Q_estados" ).change(function() {
   SqA.val("1");
   SqR.val("2");
 });
+
 $("#E_alfabeto").change(function(){
 	var S_alf=$('#S_alf');
 	S_alf.empty();
@@ -81,6 +88,7 @@ $("#E_alfabeto").change(function(){
 	};
 
 });
+
 $("#T_cinta_alf").change(function() {
 	var T_cinta_alf=$("#talf");
 	T_cinta_alf.empty();
@@ -93,5 +101,4 @@ $("#T_cinta_alf").change(function() {
 	for (var i = 0; i<S.length; i++) {
 		T_cinta_alf.append(new Option(""+S[i],i));
 	};
-
 });
